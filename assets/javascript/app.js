744ecd4e9d478c5665d97b75f871e133f0bef566
Matthew Nagy <mattn@Matts-MBP.triangle-mls.biz>
2017-10-09 15:04:42 -0400

    clear input fields

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,11 +10,13 @@
     messagingSenderId: "1021546180181"
   };
   firebase.initializeApp(config);
+  // set database variable
 var database = firebase.database();
 
+// on submit button click, capture values and write to firebase, then clear input fields
 $(document).on("click", "button", function() {
   event.preventDefault();
-
+  // capture variables
   var trainName = $("#trainName").val().trim();
   var dest = $("#dest").val().trim();
   var trainTime = $("#trainTime").val().trim();
@@ -26,7 +28,7 @@ $(document).on("click", "button", function() {
   var tMinutesTillTrain = freq - tRemainder;
   var nextTrain = moment().add(tMinutesTillTrain, "minutes");
   console.log("nexttrain" + moment(nextTrain));
-
+  // create object for firebase
   var newTrain = {
     trainName: trainName,
     dest: dest,
@@ -34,15 +36,25 @@ $(document).on("click", "button", function() {
     tMinutesTillTrain: tMinutesTillTrain,
     freq: freq
   }
+  // push object to firebase
   database.ref().push(newTrain);
+
+// clear input fields
+$("#trainName").val("");
+$("#dest").val("");
+$("#trainTime").val("");
+$("#freq").val("");
 });
 
+// write table values based on firebase childSnapshot function
 database.ref().on("child_added", function(childSnapshot, prevChildKey){
+  // capture variables from firebase data
   var trainName = childSnapshot.val().trainName;
   var dest = childSnapshot.val().dest;
   var nextTrain = childSnapshot.val().nextTrain;
   var tMinutesTillTrain = childSnapshot.val().tMinutesTillTrain;
   var freq = childSnapshot.val().freq;
   console.log(trainName)
+  // write html
   $("#train-schedule > tbody").append("<tr><td>" + trainName + "</td><td>" + dest + "</td><td>" + freq + "</td><td>" + nextTrain + "</td><td>" + tMinutesTillTrain + "</td></tr>")
 });
